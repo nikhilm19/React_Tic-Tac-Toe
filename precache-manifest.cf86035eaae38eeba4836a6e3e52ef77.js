a7abea514ecce3b18acce4acdc75ff10dcd0781e
nikmul19 <nikmul19@gmail.com>
2020-04-14 13:28:10 +0530

    Updates

---
+++ precache-manifest.cf86035eaae38eeba4836a6e3e52ef77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f704cd7d7961bfe0d7d36308852946d",
+    "url": "/React_Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "86b5bd53562a388a6397",
+    "url": "/React_Tic-Tac-Toe/static/css/main.5c3d10ca.chunk.css"
+  },
+  {
+    "revision": "05488b63e2fcd0220cd9",
+    "url": "/React_Tic-Tac-Toe/static/js/2.071dacad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Tic-Tac-Toe/static/js/2.071dacad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b5bd53562a388a6397",
+    "url": "/React_Tic-Tac-Toe/static/js/main.894f6fc1.chunk.js"
+  },
+  {
+    "revision": "51cc72a55ffd502d12a4",
+    "url": "/React_Tic-Tac-Toe/static/js/runtime-main.cda8e161.js"
+  }
+]);
